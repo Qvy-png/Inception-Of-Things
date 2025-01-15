b6bf6ced5fd062bc80fb0b56964b87c183d95994
iot <iot@iot>
2025-01-15 18:00:53 +0100

    completing scripts, though not fully working

--- p1/worker.sh
+++ p1/worker.sh
@@ -1,7 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sudo apt update -y && sudo apt upgrade -y
+# Deploy keys to allow all nodes to connect each others as root
+mkdir -p /root/.ssh
+mv /tmp/id_rsa*  /root/.ssh/
 
-sudo apt install kubectl -y
+chmod 400 /root/.ssh/id_rsa*
+chown root:root  /root/.ssh/id_rsa*
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='agent' K3S_URL=https://192.168.1.100:6443 K3S_TOKEN=aaaaa::server:3289azer sh -
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/authorized_keys
+chown root:root /root/.ssh/authorized_keys
+
+# Add current node in  /etc/hosts
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
+
+# Add kubemaster1 in  /etc/hosts
+echo "192.168.42.110  oouklichS" >> /etc/hosts
+
+echo "Installing k3s(agent) v1.21.4+k3s1..."
+scp -o StrictHostKeyChecking=no root@oouklichs:/var/lib/rancher/k3s/server/token /tmp/token
+export INSTALL_K3S_VERSION=v1.21.4+k3s1
+export INSTALL_K3S_EXEC="agent --server https://rdel-aguS:6443 --token-file /tmp/token --node-ip=192.168.42.111"
+curl -sfL https://get.k3s.io | sh -
+
+echo "Setting up aliases"
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+echo "alias c='clear'" >> /home/vagrant/.bashrc
