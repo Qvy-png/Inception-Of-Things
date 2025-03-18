c00a52394883ca66179e06ddab2ce561b86c5e09
qvy <delaguilarobin5@gmail.com>
2025-03-18 16:45:00 +0100

    adding basic makefile

--- p3/scripts/launch.sh
+++ p3/scripts/launch.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+K3D_PATH="/usr/local/bin/k3d"
+KUBECTL_PATH="/usr/local/bin/kubectl"
+ARGOCD_PATH="/usr/local/bin/argocd"
+
 if [ "$(id -u)" -ne 0 ] && [ -z "$SUDO_UID" ] && [ -z "$SUDO_USER" ]; then
 	printf "${RED}[LINUX]${NC} - Permission denied. Please run the command with sudo privileges.\n"
 	exit 87
 fi
 
-echo "[LAUNCH_SH]Configuring . . .\n"
+echo -e "${GREEN}[LAUNCH_SH]${NC} - Configuring . . ."
+
