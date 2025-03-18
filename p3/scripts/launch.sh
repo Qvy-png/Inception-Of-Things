d837b370b3b365a62afdc71419e1b936211b46aa
qvy <delaguilarobin5@gmail.com>
2025-03-18 20:42:15 +0100

    pushing launch.sh

--- p3/scripts/launch.sh
+++ p3/scripts/launch.sh
@@ -9,6 +9,8 @@ K3D_PATH="/usr/local/bin/k3d"
 KUBECTL_PATH="/usr/local/bin/kubectl"
 ARGOCD_PATH="/usr/local/bin/argocd"
 
+USERNAME=$(whoami)
+
 if [ "$(id -u)" -ne 0 ] && [ -z "$SUDO_UID" ] && [ -z "$SUDO_USER" ]; then
 	printf "${RED}[LINUX]${NC} - Permission denied. Please run the command with sudo privileges.\n"
 	exit 87
@@ -16,3 +18,14 @@ fi
 
 echo -e "${GREEN}[LAUNCH_SH]${NC} - Configuring . . ."
 
+echo -e "${YELLOW}[LAUNCH_SH]${NC} - starting with k3d"
+
+if sudo k3d cluster list | grep -q "$USERNAME"; then
+	printf "${RED}[LAUNCH_SH]${NC} - A cluster named $USERNAME already exists.\n"
+	exit 1
+else
+	if ! sudo k3d cluster create $USERNAME --port 80:80 --servers 1 --agents 3; then
+        echo -e "${RED}[LAUNCH_SH]${NC} - Cluster creation failed! Do you have k3d installed and is the Docker service running?${NC}"
+        exit 1
+    fi
+fi
